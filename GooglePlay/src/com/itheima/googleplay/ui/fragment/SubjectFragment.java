2669c1ebb3ce3a883dfd21239fa95a9a34316603
18622432060 <251370165@qq.com>
2017-03-22 11:51:01 +0800

    ff

---
+++ GooglePlay/src/com/itheima/googleplay/ui/fragment/SubjectFragment.java
@@ -0,0 +1,59 @@
+package com.itheima.googleplay.ui.fragment;
+
+import java.util.ArrayList;
+
+import com.itheima.googleplay.domain.SubjectInfo;
+import com.itheima.googleplay.http.protocol.SubjectProtocol;
+import com.itheima.googleplay.ui.adapter.MyBaseAdapter;
+import com.itheima.googleplay.ui.holder.BaseHolder;
+import com.itheima.googleplay.ui.holder.SubjectHolder;
+import com.itheima.googleplay.ui.view.MyListView;
+import com.itheima.googleplay.ui.view.LoadingPage.ResultState;
+import com.itheima.googleplay.utils.UIUtils;
+
+import android.view.View;
+
+/**
+ * 专题
+ * 
+ * @author Kevin
+ * @date 2015-10-27
+ */
+public class SubjectFragment extends BaseFragment {
+
+	private ArrayList<SubjectInfo> data;
+
+	@Override
+	public View onCreateSuccessView() {
+		MyListView view = new MyListView(UIUtils.getContext());
+		view.setAdapter(new SubjectAdapter(data));
+		return view;
+	}
+
+	@Override
+	public ResultState onLoad() {
+		SubjectProtocol protocol = new SubjectProtocol();
+		data = protocol.getData(0);
+		return check(data);
+	}
+
+	class SubjectAdapter extends MyBaseAdapter<SubjectInfo> {
+
+		public SubjectAdapter(ArrayList<SubjectInfo> data) {
+			super(data);
+		}
+
+		@Override
+		public BaseHolder<SubjectInfo> getHolder(int position) {
+			return new SubjectHolder();
+		}
+
+		@Override
+		public ArrayList<SubjectInfo> onLoadMore() {
+			SubjectProtocol protocol = new SubjectProtocol();
+			ArrayList<SubjectInfo> moreData = protocol.getData(getListSize());
+			return moreData;
+		}
+
+	}
+}
